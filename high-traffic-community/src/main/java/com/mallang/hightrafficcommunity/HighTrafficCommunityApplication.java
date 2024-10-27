6e7223bbc27f9b1cd6bbecd8ac574688edee3d7e
sunyeon-Jeong <sunyeon4012@gmail.com>
2024-10-28 07:27:26 +0900

    Set : 개발환경구축 #1 - project build

---
+++ high-traffic-community/src/main/java/com/mallang/hightrafficcommunity/HighTrafficCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.mallang.hightrafficcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighTrafficCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighTrafficCommunityApplication.class, args);
+    }
+
+}
---
+++ high-traffic-community/src/test/java/com/mallang/hightrafficcommunity/HighTrafficCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallang.hightrafficcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighTrafficCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
